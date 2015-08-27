2a2636ffbdb56a9794ae6c247278a8027a1879fa
Simon Morley <simon@polkaspots.com>
2015-08-28 00:32:59 +0100

    Testing the other repo to see if compiles

--- src/superman.c
+++ src/superman.c
@@ -13,8 +13,31 @@
 /* #include <time.h> */
 
 /* #include "types.h" */
-#include "network.h"
+/* #include "network.h" */
 /* #include "packet.h" */
+
+#define TYPES_H_
+#define NETWORK_H_
+#define NETWORK_FLAG_WPA  (1<<0)
+#define NETWORK_FLAG_TIME (1<<1)
+
+typedef uint8_t mac_t[6];
+
+struct network_t {
+	char ssid[33]; /* ESSID name (0-terminated string) */
+	mac_t mac;
+	mac_t dst;
+	uint16_t seq;
+	uint8_t channel;
+	uint8_t flags;
+	struct network_t *next;
+};
+
+struct network_t *network_add(struct network_t **list, char *ssid, mac_t m, mac_t d, uint8_t flags);
+
+int network_count(struct network_t **list);
+
+struct network_t *network_find(struct network_t **list, char *ssid);
 
 static uint8_t verbose = 0;
 
