25ca437f47f025f9897f9a366d0cc82fa7fefe0c
Simon Morley <simon@polkaspots.com>
2015-08-27 20:41:13 +0100

    gitignore

--- src/superman.c
+++ src/superman.c
@@ -1,10 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <pcap.h>
+
+#define SNAP_LEN 1518
 
 int main(int argc, char *argv[])
 {
   char *dev = argv[1];
+  pcap_t *handle;
+  char errbuf[PCAP_ERRBUF_SIZE];
 
   printf("Device: %s\n", dev);
-  return(0);
+
+  handle = pcap_open_live(dev, SNAP_LEN, 1, 1000, errbuf);
+  if (handle == NULL) {
+    fprintf(stderr, "Couldn't open device %s: %s\n", dev, errbuf);
+    exit(EXIT_FAILURE);
+  }
+
 }
