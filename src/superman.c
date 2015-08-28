6486c5d62d86b2326cccce38c98940f75ff251ec
Simon Morley <simon@polkaspots.com>
2015-08-28 16:33:41 +0100

    Refactor

--- src/superman.c
+++ src/superman.c
@@ -1,7 +1,6 @@
 /*
  * Playing with probes from this repo
  * https://github.com/wertarbyte/blighthouse
- * https://github.com/Joel-Schofield/RSSI-Sniffer
  */
 
 #include <stdio.h>
@@ -11,16 +10,25 @@
 #include <unistd.h>
 #include <string.h>
 #include <ctype.h>
-/* #include <net/ethernet.h> */
+
+/* #include <signal.h> */
+/* #include <netinet/ip.h> */
+/* #include <sys/time.h> */
+#include <net/ethernet.h>
+/* #include <pthread.h> */
 
 typedef uint8_t mac_t[6];
 
-#define MESSAGE_BUFF_LEN                        200
-#define NODE_ID_LEN                             200
+static uint8_t verbose = 0;
+static uint8_t timestamp[8] = {0xFF};
+
+/* static mac_t ap_base_mac = {0x02, 0xDE, 0xAD, 0xBE, 0xEF, 0x42}; */
+/* static mac_t brd_mac     = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF}; */
+/* static mac_t dest_mac    = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF}; */
 
 void client_mac(const mac_t m) {
-  printf("%s ", m);
-  /* printf("%02hhX:%02hhX:%02hhX:%02hhX:%02hhX:%02hhX\n", m[0], m[1], m[2], m[3], m[4], m[5]); */
+  /* printf("%s ", m); */
+  printf("%02hhX:%02hhX:%02hhX:%02hhX:%02hhX:%02hhX\n", m[0], m[1], m[2], m[3], m[4], m[5]);
   /* printf("%02X:%02X:%02X:%02X:%02X:%02X\n", m[5], m[4], m[3], m[2], m[1], m[0]); */
 }
 
@@ -34,119 +42,42 @@ static char *append_str(char *buf, char *data) {
   return append_to_buf(buf, data, size);
 }
 
+void get_essid(char *essid, const uint8_t *p, const size_t max_psize) {
+  const uint8_t *end = p+max_psize;
+  p += 4; //+6+6+6+2;
+  while (p < end) {
+    if (*p == 0x00) {
+      /* if (p[1] == 0) { */
+
+      /* } else { */
+        strncpy(essid, &p[2], p[1]);
+      /* } */
+      essid[p[1]] = '\0';
+      break;
+    } else {
+      p += 1+p[1];
+    }
+  }
+}
+
 struct ieee80211_radiotap_header {
   u_int8_t it_pad;
   u_int32_t it_present;
   u_int16_t it_len;       
 };
 
-//ethernet packet header.
-typedef struct {
-  unsigned short                  fc;             /* frame control */
-  unsigned short                  durid;          /* duration/ID */
-  u_char  a1[6];          /* address 1 */
-  u_char  a2[6];          /* address 2 */
-  u_char  a3[6];          /* address 3 */
-  unsigned short                  seq;            /* sequence control */
-  u_char  a4[6];          /* address 4 */
-} __attribute__((__packed__)) dot11_header;
+void pcap_callback(u_char *bp, const struct pcap_pkthdr *header, const uint8_t *packet) {
 
-volatile u_int8_t wifi_channel;
-char node_id[NODE_ID_LEN];
+  char essid[0xFF];
+  struct ieee80211_radiotap_header *rh =(struct ieee80211_radiotap_header *)packet;
+  uint16_t rt_length = (packet[2] | (uint16_t)packet[3]>>8);
+  const uint8_t *p = &packet[rt_length];
+  client_mac(&p[4]);
+  printf("len: %i\n", rt_length);
+  printf("lll: %i\n", rh->it_len);
 
-void print_mac(FILE * stream,u_char * mac);
-
-void pcap_callback(u_char *args, const struct pcap_pkthdr *header, const u_char *packet) {
-
-  //quickly grab the wifi channel incase it changes while processing the packet.
-  u_int8_t packet_channel = wifi_channel;
-
-  char messageBuff[MESSAGE_BUFF_LEN]; //buffer for storing a message to be sent.
-
-  int err;
-  int radiotap_header_len;
-  int8_t rssi;
-  /* struct ieee80211_radiotap_iterator iter; */
-
-  /* err = ieee80211_radiotap_iterator_init(&iter, (void*)packet, 25, &vns); */
-  /* if (err == 0) */ 
-  /*   fprintf(stdout, "all good!\n"); */
-  /* else */ 
-  /*   fprintf(stdout, "all bad! %d\n", err); */
-
-  //extract the length of the header.
-  /* radiotap_header_len = iter._max_length; */ 
-
-  //sanity printf of header length.
-  printf("header length: %d\n", radiotap_header_len);
-
-  //loop through the packet, looking for the desired data (rssi)
-  /* while (!(err = ieee80211_radiotap_iterator_next(&iter))) { */
-
-  /*   if (iter.this_arg_index == IEEE80211_RADIOTAP_DBM_ANTSIGNAL) { */
-  /*     rssi = (int8_t)iter.this_arg[0]; */
-  /*     printf("antsignal is: %d\n", rssi); */
-  /*   } */
-  /* } */
-
-  //cast received packet into ethernet packet header. the size of the radiotap header can change, hence cannot
-  //be cast statically. 
-  dot11_header * dot_head = (dot11_header*) (packet + radiotap_header_len * sizeof(char) );
-  printf("dest: "); client_mac(dot_head->a1);
-  /* printf("src:"); print_mac(stdout, dot_head->a2); printf("\n"); */
-
-  /*
-     for (int i=0; i < 64; i++) {
-     if (i%4 == 0 && i != 0)
-     fprintf(stdout, "\n");
-     fprintf(stdout, "%0.2x", packet[i]);
-     }
-     fprintf(stdout, "\n");
-     */
-
-
-  /* sprintf(messageBuff, "{\"node_id\":\"%s\",\"channel\":%d,\"rssi\":%d,\"macSrc\":\"%.2x:%.2x:%.2x:%.2x:%.2x:%.2x\"}", */ 
-  /*     node_id, */
-  /*     packet_channel, */
-  /*     rssi, */ 
-  /*     dot_head->a2[0], */
-  /*     dot_head->a2[1], */
-  /*     dot_head->a2[2], */
-  /*     dot_head->a2[3], */
-  /*     dot_head->a2[4], */
-  /*     dot_head->a2[5] */
-  /*     ); */
-
-};
-
-void print_mac(FILE * stream,u_char * mac) {
-  /* for (int i=0; i < 6; i++) { */
-  /*   fprintf(stream, "%.2x", mac[i]); */
-  /* } */
-  int i = 0;
-  for (i=1; i <= 6; i++)
-    printf("%.2x", mac[i]);
-}
-
-/* void print_mac(FILE * stream,u_char * mac) { */
-/*         for (int i=0; i < 6; i++) { */
-/*                 fprintf(stream, "%.2x", mac[i]); */
-/*         } */
-/* } */
-
-
-void xxxpcap_callback(u_char *bp, const struct pcap_pkthdr *header, const uint8_t *packet) {
-
-  /* char essid[0xFF]; */
-  /* struct ieee80211_radiotap_header *rh =(struct ieee80211_radiotap_header *)packet; */
-  /* uint16_t rt_length = (packet[2] | (uint16_t)packet[3]>>8); */
-  /* const uint8_t *p = &packet[rt_length]; */
-  /* client_mac(&p[4]); */
-  /* printf("len: %i\n", rt_length); */
-  /* printf("lll: %i\n", rh->it_len); */
-
-  /* get_essid(essid, p, header->caplen); */
-  /* printf("ssss: %s\n", essid); */
+  get_essid(essid, p, header->caplen);
+  printf("ssss: %s\n", essid);
 
 };
 
@@ -209,15 +140,15 @@ int main(int argc, char *argv[]) {
   int  c;
   opterr = 0;
 
-  while ((c = getopt(argc, argv, "i:")) != -1) {
+  while ((c = getopt(argc, argv, "i:v")) != -1) {
     switch(c) {
       case 'i':
         printf("Listen on interface %s\n", optarg);
         if_name = optarg;
         break;
-      /* case 'v': */
-      /*   verbose = 1; */
-      /*   break; */
+      case 'v':
+        verbose = 1;
+        break;
       default:
         abort();
     }
