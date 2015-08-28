fb860ad754ff1a6b009396f8f5787c76eec02608
simonm <simonm@debian>
2015-08-28 01:43:36 +0100

    Mon0

--- src/superman.c
+++ src/superman.c
@@ -15,13 +15,17 @@
 /* #include "packet.h" */
 
 #define NETWORK_H_
-
-#include <stdint.h>
-
-#include "types.h"
-
 #define NETWORK_FLAG_WPA  (1<<0)
 #define NETWORK_FLAG_TIME (1<<1)
+
+typedef uint8_t mac_t[6];
+
+static uint8_t verbose = 0;
+static uint8_t timestamp[8] = {0xFF};
+
+static mac_t ap_base_mac = {0x02, 0xDE, 0xAD, 0xBE, 0xEF, 0x42};
+static mac_t brd_mac     = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
+static mac_t dest_mac    = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
 
 struct network_t {
         char ssid[33]; /* ESSID name (0-terminated string) */
@@ -35,29 +39,55 @@ struct network_t {
 
 struct network_t *network_add(struct network_t **list, char *ssid, mac_t m, mac_t d, uint8_t flags);
 
-int network_count(struct network_t **list);
-
 struct network_t *network_find(struct network_t **list, char *ssid);
 
-typedef uint8_t mac_t[6];
+struct network_t *network_add(struct network_t **list, char *ssid, mac_t m, mac_t d, uint8_t flags) {
+        while (*list) {
+                list = &(*list)->next;
+        }
+        *list = malloc(sizeof(**list));
+        strncpy((*list)->ssid, ssid, sizeof((*list)->ssid));
+        (*list)->ssid[32] = '\0';
+        memcpy((*list)->mac, m, sizeof(mac_t));
+        memcpy((*list)->dst, d, sizeof(mac_t));
+        (*list)->seq = 0;
+        (*list)->flags = flags;
+        (*list)->next = NULL;
+}
 
-static uint8_t verbose = 0;
-
-static mac_t ap_base_mac = {0x02, 0xDE, 0xAD, 0xBE, 0xEF, 0x42};
-static mac_t brd_mac     = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
-
-static mac_t dest_mac    = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
+struct network_t *network_find(struct network_t **list, char *ssid) {
+        /* while (*list) { */
+        /*         if (strcmp(ssid, (*list)->ssid) == 0) { */
+        /*                 return *list; */
+        /*         } */
+        /*         list = &(*list)->next; */
+        /* } */
+        return NULL;
+}
 
 static struct network_t *network_list = NULL;
 
-/* void print_mac(const mac_t m) { */
-/*   printf("%02hhx:%02hhx:%02hhx:%02hhx:%02hhx:%02hhx", m[0], m[1], m[2], m[3], m[4], m[5]); */
-/* } */
+int network_count(struct network_t **list);
+int build_beacon(char *buf, struct network_t *n);
 
-/* int read_mac(char *arg, mac_t d) { */
-/*         int r = sscanf(arg, "%hhx:%hhx:%hhx:%hhx:%hhx:%hhx", &d[0], &d[1], &d[2], &d[3], &d[4], &d[5]); */
-/*         return (r != sizeof(mac_t)); */
-/* } */
+int network_count(struct network_t **list) {
+        int i = 0;
+        /* while (*list) { */
+        /*         list = &(*list)->next; */
+        /*         i++; */
+        /* } */
+        return i;
+}
+
+
+void print_mac(const mac_t m) {
+  printf("%02hhx:%02hhx:%02hhx:%02hhx:%02hhx:%02hhx", m[0], m[1], m[2], m[3], m[4], m[5]);
+}
+
+int read_mac(char *arg, mac_t d) {
+        int r = sscanf(arg, "%hhx:%hhx:%hhx:%hhx:%hhx:%hhx", &d[0], &d[1], &d[2], &d[3], &d[4], &d[5]);
+        return (r != sizeof(mac_t));
+}
 
 static char *append_to_buf(char *buf, char *data, int size) {
         memcpy(buf, data, size);
@@ -77,38 +107,38 @@ int build_beacon(char *buf, struct network_t *n) {
         b += 8;
         b = append_to_buf(b, "\x80\x00\x00\x00", 4); /* IEEE802.11 beacon frame */
         b = append_to_buf(b, n->dst, sizeof(mac_t)); /* destination */
-        /* b = append_to_buf(b, n->mac, sizeof(mac_t)); /1* source *1/ */
-        /* b = append_to_buf(b, n->mac, sizeof(mac_t)); /1* BSSID *1/ */
-        /* /1* sequence number *1/ */
-        /* *(b++) = n->seq >> 8; */
-        /* *(b++) = n->seq & 0x00FF; */
-        /* n->seq++; */
-        /* b = append_to_buf(b, timestamp, sizeof(timestamp)); /1* time stamp *1/ */
-        /* b = append_to_buf(b, "\x64\x00", 2); /1* beacon interval *1/ */
-        /* b = append_to_buf(b, "\x01\x04", 2); /1* capabilities *1/ */
+        b = append_to_buf(b, n->mac, sizeof(mac_t)); /* source */
+        b = append_to_buf(b, n->mac, sizeof(mac_t)); /* BSSID */
+        /* sequence number */
+        *(b++) = n->seq >> 8;
+        *(b++) = n->seq & 0x00FF;
+        n->seq++;
+        b = append_to_buf(b, timestamp, sizeof(timestamp)); /* time stamp */
+        b = append_to_buf(b, "\x64\x00", 2); /* beacon interval */
+        b = append_to_buf(b, "\x01\x04", 2); /* capabilities */
 
-        /* *(b++) = 0; /1* tag essid *1/ */
-        /* *(b++) = strlen(n->ssid); */
-        /* b = append_str(b, n->ssid); */
+        *(b++) = 0; /* tag essid */
+        *(b++) = strlen(n->ssid);
+        b = append_str(b, n->ssid);
 
-        /* b = append_to_buf(b, "\x01\x01\x82", 3); /1* We only support 1 MBit *1/ */
-        /* b = append_to_buf(b, "\x03\x01", 2); /1* the channel we are curently on... *1/ */
-        /* *(b++) = n->channel; */
+        b = append_to_buf(b, "\x01\x01\x82", 3); /* We only support 1 MBit */
+        b = append_to_buf(b, "\x03\x01", 2); /* the channel we are curently on... */
+        *(b++) = n->channel;
 
-        /* /1* WPA tags *1/ */
-        /* if (n->flags & NETWORK_FLAG_WPA) { */
-        /*         b = append_to_buf(b, "\x30", 1); */
-        /*         b = append_to_buf(b, "\x14", 1); /1* tag length *1/ */
-        /*         b = append_to_buf(b, "\x01\x00", 2); /1* version *1/ */
-        /*         b = append_to_buf(b, "\x00\x0f\xac", 3); /1* cipher suite OUI *1/ */
-        /*         b = append_to_buf(b, "\x02", 1); /1* TKIP *1/ */
-        /*         b = append_to_buf(b, "\x01\x00", 2); /1* cipher suite count *1/ */
-        /*         b = append_to_buf(b, "\x00\x0f\xac\x02", 4); /1* pairwire cipher suite list *1/ */
-        /*         b = append_to_buf(b, "\x01\x00", 2); /1* auth key management suite count *1/ */
-        /*         b = append_to_buf(b, "\x00\x0f\xac\x02", 4); /1* auth key management list *1/ */
-        /*         b = append_to_buf(b, "\x00\x00", 2); /1* RSN capabilities *1/ */
-        /* } */
-        /* return (b-buf); */
+        /* WPA tags */
+        if (n->flags & NETWORK_FLAG_WPA) {
+                b = append_to_buf(b, "\x30", 1);
+                b = append_to_buf(b, "\x14", 1); /* tag length */
+                b = append_to_buf(b, "\x01\x00", 2); /* version */
+                b = append_to_buf(b, "\x00\x0f\xac", 3); /* cipher suite OUI */
+                b = append_to_buf(b, "\x02", 1); /* TKIP */
+                b = append_to_buf(b, "\x01\x00", 2); /* cipher suite count */
+                b = append_to_buf(b, "\x00\x0f\xac\x02", 4); /* pairwire cipher suite list */
+                b = append_to_buf(b, "\x01\x00", 2); /* auth key management suite count */
+                b = append_to_buf(b, "\x00\x0f\xac\x02", 4); /* auth key management list */
+                b = append_to_buf(b, "\x00\x00", 2); /* RSN capabilities */
+        }
+        return (b-buf);
 }
 
 void get_essid(char *essid, const uint8_t *p, const size_t max_psize) {
@@ -137,17 +167,17 @@ void process_probe(u_char *user, const struct pcap_pkthdr *h, const uint8_t *b) 
   get_essid(essid, p, h->caplen);
   if (verbose) {
     printf("Incoming request\n");
-    /* printf("DST: "); print_mac(&p[4]); printf("\n"); */
-    /* printf("SRC: "); print_mac(&p[4+6]); printf("\n"); */
-    /* printf("BSS: "); print_mac(&p[4+6+6]); printf("\n"); */
-    /* printf("SSID <%s>\n", essid); */
+    printf("DST: "); print_mac(&p[4]); printf("\n");
+    printf("SRC: "); print_mac(&p[4+6]); printf("\n");
+    printf("BSS: "); print_mac(&p[4+6+6]); printf("\n");
+    printf("SSID <%s>\n", essid);
   }
-  /* struct network_t *n = network_find(&network_list, essid); */
-  /* if (n) { */
-  /*   printf("Incoming probe from "); */
-  /*   /1* print_mac(&p[4+6]); *1/ */
-  /*   printf(" for ssid <%s>\n", essid); */
-  /* } */
+  struct network_t *n = network_find(&network_list, essid);
+  if (n) {
+    printf("Incoming probe from ");
+    print_mac(&p[4+6]);
+    printf(" for ssid <%s>\n", essid);
+  }
 }
 
 int main(int argc, char *argv[]) {
@@ -210,10 +240,10 @@ int main(int argc, char *argv[]) {
       /* strftime(nw->ssid, 32, "%Y-%m-%d %H:%M", tmp); */
     }
     int buffersize = build_beacon(beacon, nw);
-    /* int s = pcap_inject(pcap, beacon, buffersize); */
+    int s = pcap_inject(pcap, beacon, buffersize);
 
     /* if (verbose) { */
-    /*   printf("sending beacon '%s'", nw->ssid); */
+      printf("sending beacon '%s'", nw->ssid);
     /*   printf(" (AP: %02hhx:%02hhx:%02hhx:%02hhx:%02hhx:%02hhx)", nw->mac[0], nw->mac[1], nw->mac[2], nw->mac[3], nw->mac[4], nw->mac[5]); */
     /*   printf("\n"); */
     /* } */
