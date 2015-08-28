a25dd5e1a343ba10de8eea7d3b8b8ced6ff10ab3
Simon Morley <simon@polkaspots.com>
2015-08-28 12:10:24 +0100

    Refactor

--- src/superman.c
+++ src/superman.c
@@ -92,7 +92,7 @@ void pcap_callback(u_char *bp, const struct pcap_pkthdr *header, const uint8_t *
   /* for (i=1; i <= ETH_ALEN; i++) */
   /*   printf("%02x ", ehdr.ether_dhost[ETH_ALEN-i] ); */
   
-  client_mac(ehdr.ether_dhost);
+  client_mac(ehdr.ether_shost);
   /* printf("%02x ", ehdr.ether_dhost[4] ); */
   /* printf("\n"); */
 
