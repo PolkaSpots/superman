c5131b407aa9c4c52dc2f2dc98478ca20f7e5c7e
Simon Morley <simon@polkaspots.com>
2015-08-28 18:11:44 +0100

    Refactor

--- src/superman.c
+++ src/superman.c
@@ -130,6 +130,7 @@ void pcap_callback(u_char *args, const struct pcap_pkthdr *header, const u_char 
         dot11_header * dot_head = (dot11_header*) (packet + radiotap_header_len * sizeof(char) );
         printf("dest: "); print_mac(stdout, dot_head->a1); printf("\n");
         printf("src:"); print_mac(stdout, dot_head->a2); printf("\n");
+        printf("rssi:", rssi); printf("\n");
 
         /*
         for (int i=0; i < 64; i++) {
