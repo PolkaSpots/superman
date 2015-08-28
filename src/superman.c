d56b2d17363072f009d27f62c0e8818af674bcfc
simonm <simonm@debian>
2015-08-28 03:19:38 +0100

    Mon0

--- src/superman.c
+++ src/superman.c
@@ -10,9 +10,6 @@
 #include <unistd.h>
 #include <string.h>
 #include <ctype.h>
-/* #include "types.h" */
-/* #include "network.h" */
-/* #include "packet.h" */
 
 #define NETWORK_H_
 #define NETWORK_FLAG_WPA  (1<<0)
@@ -99,48 +96,6 @@ static char *append_str(char *buf, char *data) {
         return append_to_buf(buf, data, size);
 }
 
-int build_beacon(char *buf, struct network_t *n) {
-        char *b = buf;
-        /* prepend a minimal radiotap header */
-        memset(b, 0x00, 8);
-        b[2] = 8;
-        b += 8;
-        b = append_to_buf(b, "\x80\x00\x00\x00", 4); /* IEEE802.11 beacon frame */
-        b = append_to_buf(b, n->dst, sizeof(mac_t)); /* destination */
-        b = append_to_buf(b, n->mac, sizeof(mac_t)); /* source */
-        b = append_to_buf(b, n->mac, sizeof(mac_t)); /* BSSID */
-        /* sequence number */
-        *(b++) = n->seq >> 8;
-        *(b++) = n->seq & 0x00FF;
-        n->seq++;
-        b = append_to_buf(b, timestamp, sizeof(timestamp)); /* time stamp */
-        b = append_to_buf(b, "\x64\x00", 2); /* beacon interval */
-        b = append_to_buf(b, "\x01\x04", 2); /* capabilities */
-
-        *(b++) = 0; /* tag essid */
-        *(b++) = strlen(n->ssid);
-        b = append_str(b, n->ssid);
-
-        b = append_to_buf(b, "\x01\x01\x82", 3); /* We only support 1 MBit */
-        b = append_to_buf(b, "\x03\x01", 2); /* the channel we are curently on... */
-        *(b++) = n->channel;
-
-        /* WPA tags */
-        if (n->flags & NETWORK_FLAG_WPA) {
-                b = append_to_buf(b, "\x30", 1);
-                b = append_to_buf(b, "\x14", 1); /* tag length */
-                b = append_to_buf(b, "\x01\x00", 2); /* version */
-                b = append_to_buf(b, "\x00\x0f\xac", 3); /* cipher suite OUI */
-                b = append_to_buf(b, "\x02", 1); /* TKIP */
-                b = append_to_buf(b, "\x01\x00", 2); /* cipher suite count */
-                b = append_to_buf(b, "\x00\x0f\xac\x02", 4); /* pairwire cipher suite list */
-                b = append_to_buf(b, "\x01\x00", 2); /* auth key management suite count */
-                b = append_to_buf(b, "\x00\x0f\xac\x02", 4); /* auth key management list */
-                b = append_to_buf(b, "\x00\x00", 2); /* RSN capabilities */
-        }
-        return (b-buf);
-}
-
 void get_essid(char *essid, const uint8_t *p, const size_t max_psize) {
   const uint8_t *end = p+max_psize;
   p += 4+6+6+3;
@@ -183,22 +138,15 @@ int main(int argc, char *argv[]) {
   pcap_errbuf[0] = '\0';
 
   char *if_name = NULL;
-  uint8_t use_wpa = 0;
-  uint8_t time_ssid = 0;
-  uint8_t listen = 1;
-  int channel = 1;
 
   int  c;
   opterr = 0;
 
-  while ((c = getopt(argc, argv, "i:v")) != -1) {
+  while ((c = getopt(argc, argv, "i:")) != -1) {
     switch(c) {
       case 'i':
         printf("Listen on interface %s\n", optarg);
         if_name = optarg;
-        break;
-      case 'v':
-        verbose = 1;
         break;
       default:
         abort();
@@ -210,21 +158,17 @@ int main(int argc, char *argv[]) {
     printf("%s\n", pcap_errbuf);
     exit(1);
   }
-  /* if (listen) { */
-    printf("Listen...");
-    struct bpf_program filter_probe_req;
-    struct bpf_program filter_probe_resp;
-    pcap_compile(pcap, &filter_probe_req, "type mgt subtype probe-req", 1, PCAP_NETMASK_UNKNOWN);
-    pcap_setfilter(pcap, &filter_probe_req);
+  printf("Listen...");
+  struct bpf_program filter_probe_req;
+  struct bpf_program filter_probe_resp;
+  pcap_compile(pcap, &filter_probe_req, "type mgt subtype probe-req", 1, PCAP_NETMASK_UNKNOWN);
+  pcap_setfilter(pcap, &filter_probe_req);
 
-    pcap_compile(pcap, &filter_probe_resp, "type mgt subtype probe-resp", 1, PCAP_NETMASK_UNKNOWN);
-    pcap_setfilter(pcap, &filter_probe_resp);
-  /* } */
-  /* int quantity = network_count(&network_list); */
+  pcap_compile(pcap, &filter_probe_resp, "type mgt subtype probe-resp", 1, PCAP_NETMASK_UNKNOWN);
+  pcap_setfilter(pcap, &filter_probe_resp);
 
   int link_layer_type = pcap_datalink(pcap);
 
-  /* printf("LL: %s\n", link_layer_type); */
   if (link_layer_type != DLT_IEEE802_11_RADIO) {
     const char *lln_pre = pcap_datalink_val_to_name(link_layer_type);
     const char *lln_req = pcap_datalink_val_to_name(DLT_IEEE802_11_RADIO);
@@ -238,38 +182,11 @@ int main(int argc, char *argv[]) {
   struct tm *tmp;
   int count = 0;
 
-  /* printf("transmitting beacons for %d network%s via '%s'", quantity, (quantity == 1 ? "" : "s"), if_name); */
-  /* printf(" to "); */
-  /* print_mac(dest_mac); */
-  /* printf("\n"); */
   struct network_t *nw = network_list;
   while (1) {
-  /*         if (nw->flags & NETWORK_FLAG_TIME) { */
-  /*                 /1* t = time(NULL); *1/ */
-  /*                 /1* tmp = localtime(&t); *1/ */
-  /*                 /1* if (!tmp) { *1/ */
-  /*                 /1*         perror("localtime"); *1/ */
-  /*                 /1*         exit(1); *1/ */
-  /*                 /1* } *1/ */
-  /*                 /1* strftime(nw->ssid, 32, "%Y-%m-%d %H:%M", tmp); *1/ */
-  /*         } */
-          /* int buffersize = build_beacon(beacon, nw); */
-          /* int s = pcap_inject(pcap, beacon, buffersize); */
-
-  /*         if (verbose) { */
-  /*                 printf("sending beacon '%s'", nw->ssid); */
-  /*                 printf(" (AP: %02hhx:%02hhx:%02hhx:%02hhx:%02hhx:%02hhx)", nw->mac[0], nw->mac[1], nw->mac[2], nw->mac[3], nw->mac[4], nw->mac[5]); */
-  /*                 printf("\n"); */
-  /*         } */
-
-          /* usleep(100000/network_count(&network_list)); */
-          /* nw = nw->next; */
-          /* if (nw == NULL) nw = network_list; */
-
-          if (listen) {
-                  pcap_dispatch(pcap, -1, &process_probe, "beacon");
-          }
+    pcap_dispatch(pcap, -1, &process_probe, "beacon");
   }
+
   pcap_close(pcap);
   return 0;
 
