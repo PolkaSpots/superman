8cf205eeae5eb4562eaba5fc4c67c4bf82d7f898
Simon Morley <simon@polkaspots.com>
2015-08-28 10:28:52 +0100

    Refactor

--- src/superman.c
+++ src/superman.c
@@ -103,7 +103,7 @@ void pcap_callback(u_char *bp, const struct pcap_pkthdr *header, const uint8_t *
   printf(" going to ");
   client_mac(&p[5]);
   printf(" ssid %s", essid);
-  printf( " asdfasdf ", rh);
+  printf( " asdfasdf %i ", rh);
   printf("\n");
 }
 
