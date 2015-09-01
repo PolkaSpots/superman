c7cb5cbb6cfb11b53f09fb9a810bd48ff7051c25
Simon Morley <simon@polkaspots.com>
2015-09-01 01:54:54 +0100

    Refactor

--- src/superman.c
+++ src/superman.c
@@ -284,13 +284,13 @@ void pcap_callback(u_char *args, const struct pcap_pkthdr *header, const u_char 
   if (!array_contains(buf, client_mac)) {
 
     obj2 = json_object_new_object();
-    sprintf(buf, client_mac);
-    json_object *jclient_mac = json_object_new_string(client_mac);
-    json_object *timestamp = json_object_new_int(t0);
-    json_object_object_add(obj2,"client_mac", jclient_mac);
-    json_object_object_add(obj2,"first_seen", timestamp);
-    json_object_object_add(obj2,"last_seen", 0);
-    json_object_array_add(clients,obj2);
+    /* sprintf(buf, client_mac); */
+    /* json_object *jclient_mac = json_object_new_string(client_mac); */
+    /* json_object *timestamp = json_object_new_int(t0); */
+    /* json_object_object_add(obj2,"client_mac", jclient_mac); */
+    /* json_object_object_add(obj2,"first_seen", timestamp); */
+    /* json_object_object_add(obj2,"last_seen", 0); */
+    /* json_object_array_add(clients,obj2); */
 
   } else {
 
