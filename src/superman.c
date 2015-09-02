541a1a5991aa87bc90cd9c2a7aaa7c65902bbf20
Simon Morley <simon@polkaspots.com>
2015-09-02 02:01:38 +0100

    Refactor

--- src/superman.c
+++ src/superman.c
@@ -287,22 +287,22 @@ void pcap_callback(u_char *args, const struct pcap_pkthdr *header, const u_char 
 
     /*   /1* } else { *1/ */
 
-    /*     /1* arraylen = json_object_array_length(clients); *1/ */
-    /*     /1* for (i = 0; i < arraylen; i++) { *1/ */
-    /*     /1*   tmp1 = json_object_array_get_idx(clients, i); *1/ */
-    /*     /1*   json_object_object_get_ex(tmp1, "client_mac", &tmp2); *1/ */
+  /*     /1* arraylen = json_object_array_length(clients); *1/ */
+  /*     /1* for (i = 0; i < arraylen; i++) { *1/ */
+  /*     /1*   tmp1 = json_object_array_get_idx(clients, i); *1/ */
+  /*     /1*   json_object_object_get_ex(tmp1, "client_mac", &tmp2); *1/ */
 
-    /*     /1*   int result = strcmp(json_object_get_string(tmp2), client_mac); *1/ */
+  /*     /1*   int result = strcmp(json_object_get_string(tmp2), client_mac); *1/ */
 
-    /*     /1*   if ( result == 0 ) { *1/ */
+  /*     /1*   if ( result == 0 ) { *1/ */
 
-    /*     /1*     json_object_object_foreach(tmp1, key, val) { *1/ */
-    /*     /1*       if (strcmp(key, "last_seen") != 0) *1/ */
-    /*     /1*         continue; *1/ */
-    /*     /1*       json_object_object_add(tmp1, key, json_object_new_int(t0)); *1/ */
-    /*     /1*       /2* break; *2/ *1/ */
-    /*     /1*     } *1/ */
-    /*     /1*     break; *1/ */
+  /*     /1*     json_object_object_foreach(tmp1, key, val) { *1/ */
+  /*     /1*       if (strcmp(key, "last_seen") != 0) *1/ */
+  /*     /1*         continue; *1/ */
+  /*     /1*       json_object_object_add(tmp1, key, json_object_new_int(t0)); *1/ */
+  /*     /1*       /2* break; *2/ *1/ */
+  /*     /1*     } *1/ */
+  /*     /1*     break; *1/ */
 
 }
 
@@ -338,11 +338,11 @@ int array_contains(char *array, char *data ) {
   /* if (len == 0 ) { */ 
   /*   return 0; */
   /* } else { */
-  /*   for(i = 0; i < len; ++i){ */
-  /*     printf("a: %p", array[i]); */
-  /*     if(strncmp(array, data, i) == 0) */
-  /*       return 1; */
-  /*   }; */
+  for(i = 0; i < len; ++i){
+    /*     printf("a: %p", array[i]); */
+    if(strncmp(array, data, i))
+      return 1;
+  };
   /* }; */
 }
 
