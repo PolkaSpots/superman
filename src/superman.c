546f150e749588bf7a80157e487d45fdec4c11ff
Simon Morley <simon@polkaspots.com>
2015-09-02 01:27:38 +0100

    Refactor

--- src/superman.c
+++ src/superman.c
@@ -68,7 +68,7 @@
 /* #include "shared.h" */
 
 #define MAC_CACHE_LEN 20
-#define MESSAGE_BUFF_LEN 360 /* 18 LEN OF MAC * 20, MAX CACHE */
+#define MESSAGE_BUFF_LEN 800 /* 18 LEN OF MAC * 20, MAX CACHE */
 #define BUZZ_SIZE 1024 /* For the config file */
 
 // Only for the ethernet tests //
